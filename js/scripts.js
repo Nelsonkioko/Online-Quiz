574569392c83b2e9242295e0af86a80358e7e3fe
NelsonKioko <kiokonelson2@gmail.com>
2019-02-22 15:50:26 +0300

    make changes to several folder

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
-$(form#form).submit(function(event) {
-    event.preventDefault();
-    var quesone = $("input:radio[name=quesone]:checked").val();
-    var questwo = $("input:radio[name=questwo]:checked").val();
-    var questhree = $("input:radio[name=questhree]:checked").val();
-    var quesfour = $("input:radio[name=quesfour]:checked").val();
-    var quesfive = $("input:radio[name=quesfive]:checked").val();
-    
+$(document).ready(function(){
+    $("input[type='button']").click(function(){
+        var quesone = parseInt($("input[name='quiz1']:checked").val());
+        var questwo = parseInt($("input[name='quiz2']:checked").val());
+        var questhree = parseInt($("input[name='quiz3']:checked").val());
+        var total = quesone + questwo + questhree;
+        document.getElementById('finalscore').innerHTML="you have scored " + total + "points";
+    });
 
 });
