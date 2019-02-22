6199bf70e85a34efd3f64699fbc6fc7182f81ded
NelsonKioko <kiokonelson2@gmail.com>
2019-02-22 12:10:28 +0300

    create a new javascript function

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(form#form).submit(function(event) {
+    event.preventDefault();
+    var quesone = $("input:radio[name=quesone]:checked").val();
+    var questwo = $("input:radio[name=questwo]:checked").val();
+    var questhree = $("input:radio[name=questhree]:checked").val();
+    var quesfour = $("input:radio[name=quesfour]:checked").val();
+    var quesfive = $("input:radio[name=quesfive]:checked").val();
+    
+
+});
