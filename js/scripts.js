f3195660a54162b6c4fd3ee119d0221bc1b717fe
NelsonKioko <kiokonelson2@gmail.com>
2019-02-22 16:21:20 +0300

    create a function

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     $("input[type='button']").click(function(){
+        
         var quesone = parseInt($("input[name='quiz1']:checked").val());
         var questwo = parseInt($("input[name='quiz2']:checked").val());
         var questhree = parseInt($("input[name='quiz3']:checked").val());
