ac82ef9837283b3757781977445f0384b236de2d
NelsonKioko <kiokonelson2@gmail.com>
2019-02-23 13:30:43 +0300

    make changes to the js

--- js/scripts.js
+++ js/scripts.js
@@ -15,9 +15,28 @@
         var total = question1 + question2 + question3 +  question4 +  question5 + question6 + question7 + question8 + question9 + question10;
         document.getElementById('finalscore').innerHTML="you have scored " + total + "%";
 */
+$(document).ready(function(){
+    $("input[type='button']").click(function(){
+        var total=0;
+        var num = 0;
+        var arr = [];
+        for(num=1;num<=10;num+=1) {
+        var questions = parseInt($("input[name='quiz' + num]:checked").val());
+        arr.push(questions);
+        for (var index = 0; index < arr.length; index += 1)
+        {
+          total += arr[index];
+          alert(arr);
+        }
 
+}
+
+        document.getElementById('finalscore').innerHTML="you have scored " + total + "points";
     });
+
 });
+
+
 $(document).ready(function(){
   // scrolldown progress bar
 $(window).scroll(function() {
@@ -31,31 +50,3 @@ $(window).scroll(function() {
 
 });
 });
-
-
-
-
-
-/*
-
-$(document).ready(function(){
-    $("input[type='button']").click(function(){
-
-        var num = 0;
-        var arr = [];
-        for(num=1;num<=10;num+=1) {
-        var questions = parseInt($("input[name='quiz' + num]:checked").val());
-        arr.push(questions);
-      }
-        var total=0;
-        for (var index = 0; index < arr.length; index += 1)
-        {
-          total += arr[index];
-        }
-
-
-        document.getElementById('finalscore').innerHTML="you have scored " + total + "points";
-    });
-
-});
-*/
